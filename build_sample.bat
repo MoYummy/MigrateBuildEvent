1a7e143db95de23f8cec6486ca61a8f38588232c
Mike-Mohong Pan <mo_yummy@sina.cn>
2017-03-27 02:36:46 -0700

    Added SampleCpp

--- build_sample.bat
+++ build_sample.bat
@@ -6,6 +6,11 @@ pushd %workspace%
 setLocal EnableDelayedExpansion
 if defined VS140COMNTOOLS call "%VS140COMNTOOLS%vsvars32.bat"
 
+msbuild.exe SampleCpp\SampleCpp.sln /p:Configuration=Release /p:Platform=x64 > %workspace%\BuildSampleCpp.sln.Optimize.x64.!Inve.log 2>&1
+msbuild.exe SampleCpp\SampleCpp.sln /p:Configuration=Debug /p:Platform=x64 > %workspace%\BuildSampleCpp.sln.Debug.x64.!Inve.log 2>&1
+msbuild.exe SampleCpp\SampleCpp.sln /p:Configuration=Release /p:Platform=x86 > %workspace%\BuildSampleCpp.sln.Optimize.x86.!Inve.log 2>&1
+msbuild.exe SampleCpp\SampleCpp.sln /p:Configuration=Debug /p:Platform=x86 > %workspace%\BuildSampleCpp.sln.Debug.x86.!Inve.log 2>&1
+
 msbuild.exe SampleDotNet\SampleDotNet.sln /p:Configuration=Release /p:Platform=x64 > %workspace%\BuildSampleDotNet.sln.Optimize.x64.!Inve.log 2>&1
 msbuild.exe SampleDotNet\SampleDotNet.sln /p:Configuration=Debug /p:Platform=x64 > %workspace%\BuildSampleDotNet.sln.Debug.x64.!Inve.log 2>&1
 msbuild.exe SampleDotNet\SampleDotNet.sln /p:Configuration=Release /p:Platform=x86 > %workspace%\BuildSampleDotNet.sln.Optimize.x86.!Inve.log 2>&1
